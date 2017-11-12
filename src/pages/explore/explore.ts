ad90c1a4b861afda0a3f3dce0c52110f4b878d9c
Nikolai Nossulenko <nossulenkonikolai@gmail.com>
2017-11-12 20:40:00 +0100

    Slides toegevoegd

--- src/pages/explore/explore.ts
+++ src/pages/explore/explore.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+//import { ViewChild } from '@angular/core';
 import { ViewChild } from '@angular/core';
-import{VideoPlayer, VideoOptions} from '@ionic-native/video-player';
+import { Slides } from 'ionic-angular';
 
 
 
@@ -13,31 +14,15 @@ import{VideoPlayer, VideoOptions} from '@ionic-native/video-player';
 })
 
 export class ExplorePage {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
   
-  videoOptions: VideoOptions;
-  videoUrl: string;
-  constructor(private videoPlayer: VideoPlayer, public navCtrl: NavController, public navParams: NavParams) {
 
+  @ViewChild(Slides) slides: Slides;
+  goToSlide() {
+    this.slides.slideTo(2, 500);
   }
-  stopVideo(){
-    this.videoPlayer.close()
-    console.log("The video was stopped.")
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+    console.log('Current index is', currentIndex);
   }
-  async playVideo(){
-    try{
-      this.videoOptions = {
-        volume: 0.7
-      }
-      this.videoUrl = "https://www.youtube.com/watch?v=2vbs6yZTGmc"
-      setTimeout(()=>{
-        this.stopVideo();
-      }, 3000);
-      await this.videoPlayer.play(this.videoUrl, this.videoOptions)
-      console.log("video has completed")
-  
-    }catch(e){
-      console.error(e);
-    }
-  }
-
 }
