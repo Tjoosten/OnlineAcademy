23800563595279685eb43d631547c3c4d50b3eeb
Larzsify <larssuffys@hotmail.com>
2017-11-17 00:19:35 +0100

    added quotes functionality (not fully working)

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import firebase from 'firebase';
 @IonicPage()
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
-
+  //Reference to firebase quotes
+  QuotesRef: firebase.database.Reference = firebase.database().ref('/quotes/');
+  Quotes = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.loadQuotes();
   }
-  createQuote(Auteur: string, quote: string){
-  
+  addQuote(Author: string, Quote: string): void {
+    let newQuotesRef = this.QuotesRef.push();
+    newQuotesRef.set({
+      Auteur: Author,
+      Quote: Quote
+    });
   }
+  loadQuotes() {
+    this.QuotesRef.on('value', quoteSnapshot => {
+      let QuoteVal = quoteSnapshot.val();
+      this.Quotes.push(QuoteVal);
+      console.log(this.Quotes);
+    });
+  }
+
+
+
 }
